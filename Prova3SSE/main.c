9f0b3bc8417d55ec5ff82d8bdff3181e432b30f2
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2016-06-15 08:31:35 -0300

    Initial Commit

---
+++ Prova3SSE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Prova3SSE
+//
+//  Created by leonardo albuquerque on 15/06/16.
+//  Copyright © 2016 leonardo albuquerque. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
